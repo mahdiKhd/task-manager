9452e72433f2d4481dd8226f79aa0d5309392a62
Mahdi <khoshdellmahdi@gmail.com>
2024-04-24 01:37:37 +0330

    final changes

--- presentation/middleware/jwtAuthentication.js
+++ presentation/middleware/jwtAuthentication.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const {TOKEN_KEY} = process.env;
 
--- presentation/root.js
+++ presentation/root.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json())
